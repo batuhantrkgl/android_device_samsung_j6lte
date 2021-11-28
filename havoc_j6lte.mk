58a3f77940fd8aa480230126fc86db85f93db569
flominatorgd <github.flominatorgd@gmail.com>
2021-11-28 22:36:43 +0100

    havoc-11

--- havoc_j6lte.mk
+++ havoc_j6lte.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2019 The havocOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,12 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from j6lte device
 $(call inherit-product, device/samsung/j6lte/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j6lte
-PRODUCT_NAME := lineage_j6lte
+PRODUCT_NAME := havoc_j6lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy J6
 PRODUCT_MANUFACTURER := samsung
